64fd50de300de7052b6a2dcfdae72a7b2c4063b1
Gabriel Vianna <gabriel.rocha@isaci.org.br>
2025-11-03 12:44:48 -0300

    Database: Add view (vw_gathering_loser_pot_total)

--- database/database.sql
+++ database/database.sql
@@ -233,6 +233,26 @@ ORDER BY
 COMMENT ON VIEW gathering.vw_gathering_confra_pot_total IS
 'Displays the total accumulated confra pot, total rounds, and number of events for each gathering.';
 
+CREATE OR REPLACE VIEW gathering.vw_gathering_loser_pot_total AS
+SELECT
+    g.id AS id_gathering,
+    g.name AS gathering_name,
+    COALESCE(SUM(r.prize), 0) AS total_prize,
+    COALESCE(SUM(r.loser_pot), 0) AS total_loser_pot
+FROM
+    gathering.gathering g
+    INNER JOIN gathering.event e ON e.id_gathering = g.id
+    INNER JOIN gathering.round r ON r.id_event = e.id
+WHERE
+    r.canceled = false
+GROUP BY
+    g.id
+ORDER BY
+    g.name;
+    
+COMMENT ON VIEW gathering.vw_gathering_loser_pot_total IS
+'Summarizes the total prizes and loser pots accumulated across all events within each gathering.';
+
 CREATE OR REPLACE VIEW gathering.vw_gathering_format_total AS
 SELECT
     g.id AS id_gathering,
