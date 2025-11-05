3831be8ad28478e0c55542e82ec971b6c73b1194
Gabriel Vianna <gabriel.rocha@isaci.org.br>
2025-11-05 16:44:33 -0300

    Database: Optimize load test by removing wallet values

--- database/loads/load_test.sql
+++ database/loads/load_test.sql
@@ -1,13 +1,13 @@
 INSERT INTO gathering.player
-(id, name, wallet) VALUES
-(1, 'Anderson Dias', 0),
-(2, 'Arthur Leal', 0),
-(3, 'Cindomar Ferreira', 0),
-(4, 'Gabriel Vianna', 0),
-(5, 'Jean Benevides', 0),
-(6, 'Jhonny Dias', 0),
-(7, 'Tobias Souza', 0),
-(8, 'Valmir Vicente', 0);
+(id, name) VALUES
+(1, 'Anderson Dias'),
+(2, 'Arthur Leal'),
+(3, 'Cindomar Ferreira'),
+(4, 'Gabriel Vianna'),
+(5, 'Jean Benevides'),
+(6, 'Jhonny Dias'),
+(7, 'Tobias Souza'),
+(8, 'Valmir Vicente');
 
 INSERT INTO gathering.gathering
 (id, id_owner, name, year) VALUES
