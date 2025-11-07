97b9014d34c47fe72530a21ed0a9b33cbb6d828c
Gabriel Vianna <gabriel.viannasr@gmail.com>
2025-11-07 10:14:33 -0300

    TransactionService: Add validations

--- src/main/java/br/com/gathering/service/TransactionService.java
+++ src/main/java/br/com/gathering/service/TransactionService.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import br.com.gathering.constant.TransactionType;
 import br.com.gathering.entity.Transaction;
 import br.com.gathering.repository.TransactionRepository;
 import jakarta.persistence.EntityManager;
@@ -21,9 +22,6 @@ public class TransactionService extends AbstractService<Transaction> {
 
 	@Autowired
 	private TransactionRepository repository;
-
-	@Autowired
-	private EntityManager entityManager;
 
 	public static Sort getSort() {
 		return Sort.by(Order.asc("idGathering"), Order.asc("idPlayer"), Order.asc("createdAt"));
@@ -45,17 +43,63 @@ public class TransactionService extends AbstractService<Transaction> {
 	public Transaction save(Transaction model) {
 		model.init();
 		validate(model);
-
-		Transaction transaction = repository.save(model);
-		// Clear the persistence context
-		entityManager.clear();
-
-		// Fetch the managed entity to ensure it's in the persistence context
-		transaction = getById(transaction.getId());
-		return transaction;		
+		return model;		
 	}
 
 	private void validate(Transaction model) {
+
+		if (model.getIdGathering() == null) {
+
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID da gathering é obrigatório");
+
+		}
+		if (model.getIdPlayer() == null) {
+
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID do player é obrigatório");
+
+		}
+
+		Long idTransactionType = model.getIdTransactionType();
+
+		if (idTransactionType == null) {
+
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID do tipo de transação é obrigatório");
+
+		} else if (idTransactionType == TransactionType.INSCRICAO.getId() || idTransactionType == TransactionType.RESULTADO.getId()) {
+
+			if (model.getIdEvent() == null) {
+
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID do evento é obrigatório");
+
+			}
+
+		} else if (idTransactionType == TransactionType.DEPOSITO.getId()) {
+
+			if(model.getAmount() < 0) {
+
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Valor do depósito deve ser positivo");
+
+			}
+
+		} else if (idTransactionType == TransactionType.SAQUE.getId()) {
+
+			if(model.getAmount() >= 0) {
+
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Valor do saque deve ser negativo");
+
+			}
+
+			Double wallet = 0.0; // TODO: Consultar do repository
+
+			if (wallet + model.getAmount() < 0) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Saldo insuficiente para saque");
+			}
+
+		} else {
+
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tipo de transação não encontrada");
+
+		}
 
 	}
 
