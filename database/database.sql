62c825fe0d3cd3ea1f7a2ac9061c3ca4efd54a09
Gabriel Vianna <gabriel.viannasr@gmail.com>
2025-11-08 13:01:13 -0300

    Database: Rename columns in view (vw_gathering_confra_pot)
    Columns: events, rounds

--- database/database.sql
+++ database/database.sql
@@ -271,13 +271,13 @@ CREATE OR REPLACE VIEW gathering.vw_event_rank_count AS
     COMMENT ON VIEW gathering.vw_event_rank_count IS
     'Indicates how many players share each rank, used to distribute the loser pot.';
 
-CREATE OR REPLACE VIEW gathering.vw_gathering_confra_pot_total AS
+CREATE OR REPLACE VIEW gathering.vw_gathering_confra_pot AS
     SELECT
         g.id AS id_gathering,
         g.name AS gathering_name,
-        COUNT(e.id) AS total_events,
-        COALESCE(SUM(e.rounds), 0) AS total_rounds,
-        COALESCE(SUM(e.confra_pot), 0) AS total_confra_pot
+        COUNT(e.id) AS events,
+        COALESCE(SUM(e.rounds), 0) AS rounds,
+        COALESCE(SUM(e.confra_pot), 0) AS confra_pot
     FROM
         gathering.gathering g
         INNER JOIN gathering.event e ON e.id_gathering = g.id
@@ -286,7 +286,7 @@ CREATE OR REPLACE VIEW gathering.vw_gathering_confra_pot_total AS
     ORDER BY
         g.name;
 
-    COMMENT ON VIEW gathering.vw_gathering_confra_pot_total IS
+    COMMENT ON VIEW gathering.vw_gathering_confra_pot IS
     'Displays the total accumulated confra pot, total rounds, and number of events for each gathering.';
 
 CREATE OR REPLACE VIEW gathering.vw_gathering_loser_pot AS
