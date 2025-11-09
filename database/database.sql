d8c0dd5e647ff8cb13a4ecf621309d2bd6ecf5d8
Gabriel Vianna <gabriel.viannasr@gmail.com>
2025-11-08 23:10:53 -0300

    Database: Add  view (vw_event_summary)

--- database/database.sql
+++ database/database.sql
@@ -266,6 +266,25 @@ CREATE OR REPLACE VIEW gathering.vw_event_loser_pot AS
 COMMENT ON VIEW gathering.vw_event_loser_pot IS
 'Exibe o total de rodadas, o valor total de premiações e o valor acumulado no pote dos derrotados por evento.';
 
+CREATE OR REPLACE VIEW gathering.vw_event_summary AS
+SELECT 
+	loser.id_gathering,
+	loser.gathering_name,
+	loser.id_event, 
+	confra.players,
+	loser.rounds, 
+	loser.loser_pot,
+	confra.confra_pot,
+	loser.prize
+FROM
+	gathering.vw_event_loser_pot loser
+	INNER JOIN gathering.vw_event_confra_pot confra
+		ON confra.id_event = loser.id_event;
+
+COMMENT ON VIEW gathering.vw_event_summary IS
+'Apresenta um resumo consolidado de cada evento, unindo informações do pote da confra e do pote dos derrotados.
+Inclui o total de jogadores, rodadas, valores acumulados e premiações do evento.';
+
 CREATE OR REPLACE VIEW gathering.vw_event_player_balance AS
     WITH player_balance AS (
         SELECT
