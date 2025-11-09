343b7efd85eb7a52fd2defd2fe6773b6c9e664ab
Gabriel Vianna <gabriel.viannasr@gmail.com>
2025-11-09 18:40:56 -0300

    DashboardController: Add LogHelper

--- src/main/java/br/com/gathering/controller/DashboardController.java
+++ src/main/java/br/com/gathering/controller/DashboardController.java
@@ -2,6 +2,7 @@ package br.com.gathering.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,45 +11,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gathering.projection.RankProjection;
 import br.com.gathering.projection.gathering.FormatProjection;
+import br.com.gathering.projection.gathering.GatheringSummaryProjection;
 import br.com.gathering.projection.gathering.PlayerTransactionProjection;
 import br.com.gathering.projection.gathering.PlayerWalletProjection;
-import br.com.gathering.projection.gathering.GatheringSummaryProjection;
 import br.com.gathering.service.DashboardService;
+import br.com.gathering.util.LogHelper;
 
 @RestController
 @RequestMapping("/dashboard")
 public class DashboardController {
+
+	private static final Logger log = LogHelper.getLogger(DashboardController.class);
 
     @Autowired
     private DashboardService service;
 
     @GetMapping("/{idGathering}/wallet-balance")
     public List<PlayerWalletProjection> getWalletBalance(@PathVariable Long idGathering) {
-    	System.out.println("idGathering: " + idGathering);
+    	LogHelper.info(log, "DashboardController", "GET /wallet-balance", "idGathering", idGathering);
         return service.getWalletBalance(idGathering);
     }
 
     @GetMapping("/{idGathering}/transaction")
     public List<PlayerTransactionProjection> getPlayerTransaciton(@PathVariable Long idGathering) {
-    	System.out.println("idGathering: " + idGathering);
+    	LogHelper.info(log, "DashboardController", "GET /transaction", "idGathering", idGathering);
         return service.getPlayerTransaciton(idGathering);
     }
 
     @GetMapping("/{idGathering}/format")
     public List<FormatProjection> getFormatProjection(@PathVariable Long idGathering) {
-    	System.out.println("idGathering: " + idGathering);
+    	LogHelper.info(log, "DashboardController", "GET /format", "idGathering", idGathering);
         return service.getFormatProjection(idGathering);
     }
 
     @GetMapping("/{idGathering}/summary")
     public GatheringSummaryProjection getSummaryProjection(@PathVariable Long idGathering) {
-    	System.out.println("idGathering: " + idGathering);
+    	LogHelper.info(log, "DashboardController", "GET /summary", "idGathering", idGathering);
         return service.getSummaryProjection(idGathering);
     }
 
     @GetMapping("/{idGathering}/rank")
     public List<RankProjection> getRankProjection(@PathVariable Long idGathering) {
     	System.out.println("idGathering: " + idGathering);
+    	LogHelper.info(log, "DashboardController", "GET /rank", "idGathering", idGathering);
         return service.getRankProjection(idGathering);
     }
 
