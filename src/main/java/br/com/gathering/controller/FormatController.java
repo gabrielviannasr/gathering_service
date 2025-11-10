f30318e155bc975c49d59e5f1979a735efc52b48
Gabriel Vianna <gabriel.viannasr@gmail.com>
2025-11-09 21:39:47 -0300

    FormatController: Add LogHelper

--- src/main/java/br/com/gathering/controller/FormatController.java
+++ src/main/java/br/com/gathering/controller/FormatController.java
@@ -2,6 +2,7 @@ package br.com.gathering.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,30 +16,33 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.gathering.dto.FormatDTO;
 import br.com.gathering.entity.Format;
 import br.com.gathering.service.FormatService;
+import br.com.gathering.util.LogHelper;
 
 @RestController
 @RequestMapping("/format")
 public class FormatController {
 
+	private static final Logger log = LogHelper.getLogger(FormatController.class);
+	
 	@Autowired
 	private FormatService service;
 
 	@GetMapping
 	public List<Format> getList(Format model) {
+		LogHelper.info(log, "GET /format", "Format", model);
 		return service.getList(model);
 	}
 
 	@GetMapping("/{id}")
 	public Format getById(@PathVariable Long id) {
-		System.out.println("id: " + id);
+		LogHelper.info(log, "GET /format/{id}", "id", id);
 		return service.getById(id);
 	}
 
 	@PostMapping
 	public Format save(@RequestBody FormatDTO dto) {
-		System.out.println(dto);
 		Format model = dto.toModel();
-		System.out.println(model);
+		LogHelper.info(log, "POST /format", "payload", model);
 		return service.save(model);
 	}
 
@@ -47,7 +51,7 @@ public class FormatController {
 		System.out.println(dto);
 		Format model = dto.toModel();
 		model.setId(id);
-		System.out.println(model);
+		LogHelper.info(log, "PUT /format", "id", id, "payload", model);
 		return service.save(model);
 	}
 
